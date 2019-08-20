1467c4ec2ef6142a40ca9bac34de434bd5d04d15
roryfrench <rfrench@logikalprojects.com>
2019-08-20 17:57:01 +0100

    End of day 20190820

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import setuptools
+from os import path
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
-    name="cpm-pkg-roryfrench",
+    name="cpm_calculator",
     version="0.0.1",
     author="Rory French",
     author_email="rfrench@logikalprojects.com",
@@ -12,10 +15,18 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/roryfrench/CPM.git",
+    # py_modules=["cpm.py"],
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords='criticalpath',
+    python_requires='>=3.7',
+    install_requires=['datetime', 'workdays'],  # Optional
+     project_urls={  # Optional
+        'Source': 'https://github.com/roryfrench/CPM',
+    },
 )
